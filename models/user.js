97e19d772c95f2de91eff2e94083091401324095
William Nicholson <will98nicholson@gmail.com>
2021-06-18 14:40:13 -0500

    passport login

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// const passportLocalMongoose = require('passport-local-mongoose');
+
+const User = new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true }
+});
+
+
+
+module.exports = mongoose.model('Users', User);
