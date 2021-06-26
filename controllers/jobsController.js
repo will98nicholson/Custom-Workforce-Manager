1f480e9bf34e9303d9d091b9be0ebcd9a6534706
William Nicholson <will98nicholson@gmail.com>
2021-06-26 11:37:52 -0500

    isAdmin authentication

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,44 +1,44 @@
 const db = require('../models');
 
 module.exports = {
-    findall: (req, res)=> {
-        db.jobs.find(req.query)
-        .then((data)=> {
-            res.json(data)
-        })
-        .catch(err => console.log(err))
+    findall: (req, res) => {
+        db.Job.find(req.query)
+            .then((data) => {
+                res.json(data)
+            })
+            .catch(err => console.log(err))
     },
-    findById: (req, res)=> {
-        db.jobs.find({_id: req.params.id})
-        .then((data)=> {
-            res.json(data)
-        })
-        .catch(err => console.log(err))
-    }, 
-    findByName: (req, res)=> {
+    findById: (req, res) => {
+        db.jobs.find({ _id: req.params.id })
+            .then((data) => {
+                res.json(data)
+            })
+            .catch(err => console.log(err))
+    },
+    findByName: (req, res) => {
         db.jobs.find(req.params.name)
-        .then((data)=> {
-            res.json(data)
-        })
-        .catch(err => console.log(err))
+            .then((data) => {
+                res.json(data)
+            })
+            .catch(err => console.log(err))
     },
-    create: (req, res)=> {
+    create: (req, res) => {
         db.jobs.create(req.body)
-        .then((data)=> {
-            res.json(data)
-        })
-        .catch(err => console.log(err))
-    }, 
-    remove: (req, res)=> {
-        db.jobs.findOneAndDelete({_id: req.params.id})
-        .then(data => res.json(data))
-        .catch(err => console.log(err))
-    }, 
-    update: (req, res)=> {
-        db.jobs.findOneAndUpdate({_id: req.params.id})
-        .then((data)=> {
-            res.json(data)
-        })
-        .catch(err => console.log(err))
+            .then((data) => {
+                res.json(data)
+            })
+            .catch(err => console.log(err))
+    },
+    remove: (req, res) => {
+        db.jobs.findOneAndDelete({ _id: req.params.id })
+            .then(data => res.json(data))
+            .catch(err => console.log(err))
+    },
+    update: (req, res) => {
+        db.jobs.findOneAndUpdate({ _id: req.params.id })
+            .then((data) => {
+                res.json(data)
+            })
+            .catch(err => console.log(err))
     },
 }
