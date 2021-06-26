74edb207cbf3870042f68bb5034b79de892320b6
mkotte <74936870+mkotte@users.noreply.github.com>
2021-06-26 11:29:25 -0400

    starting Invoice/Header.js component

--- client/src/pages/Invoice.js
+++ client/src/pages/Invoice.js
@@ -3,7 +3,6 @@ import Header from "../components/Invoice/Header"
 import Table from "../components/Invoice/Table"
 import Notes from "../components/Invoice/Notes"
 
-
 function Invoice() { 
 
     return(
