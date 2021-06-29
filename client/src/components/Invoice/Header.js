c19156e81bf7cec7287d6f7d365aab3daf5fa686
mkotte <74936870+mkotte@users.noreply.github.com>
2021-06-29 16:24:07 -0400

    finished framing Header.js

--- client/src/components/Invoice/Header.js
+++ client/src/components/Invoice/Header.js
@@ -1,11 +1,70 @@
 import React, { Component } from "react";
-import EditAbleTextArea from "./EditAbleTextArea"
+
+// TODO: figure out how to model, seed and then retrieve neccessary information, then style!
 
 function Header() {
-   return(
-    <>
-        <EditAbleTextArea />
-    </>
+
+    companyInformation = [
+        {
+            "company": "",
+            "name": "",
+            "address": {
+                "streetAddress": "",
+                "city": "",
+                "state": "",
+                "zipcode":""
+            }
+        }
+    ];
+
+    billingInformation = [
+       {
+           "client": {
+               "name": "",
+               "address": {
+                "streetAddress": "",
+                "city": "",
+                "state": "",
+                "zipcode":""
+            }
+          }
+       }
+    ];
+
+    invoiceInformation = [
+        {
+            "number": "",
+            "dateCreated":"",
+            "dateDue":""
+        }
+    ]
+
+    return(
+        <>
+            <div className="companyInfo">
+                <h2>{companyInformation.company}</h2>
+                <h4>{companyInformation.address.streetAddress}</h4>
+                <h4>{companyInformation.address.city}, {companyInformation.address.state} {companyInformation.address.zipcode}</h4>
+                <h4>United States</h4>
+            </div>
+
+            <div className="invoiceHeader">
+                <h1>INVOICE</h1>
+            </div>
+
+            <div className="billingInfo">
+                <h3>Bill To:</h3>
+                <h4>{billingInformation.client.name}</h4>
+                <h4>{billingInformation.client.address.streetAddress}</h4>
+                <h4>{billingInformation.client.address.city}, {billingInformation.client.address.state} {billingInformation.client.address.zipcode}</h4>
+            </div>
+
+            <div>
+                <h4>Invoice # {invoiceInformation.number}</h4>
+                <h4>Date Issued: {invoiceInformation.dateCreated}</h4>
+                <h4>Due Date: {invoiceInformation.dateDue}</h4>
+            </div>
+        </>
    ) 
 };
 
