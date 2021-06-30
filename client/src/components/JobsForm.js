5bf98869cb8ebbd70bfe22b50870351f504e078d
Corrinne W <corrinneworden@gmail.com>
2021-06-29 22:26:39 -0400

    handleSubmit and handleInputChange added to JobsForm.js. APIFunction added to CreateJob.js. Note: though the form will say it has submitted, it does not yet add data to the database. Will need further troubleshooting.

--- client/src/components/JobsForm.js
+++ client/src/components/JobsForm.js
@@ -8,6 +8,9 @@ import {
     TextField,
     Button
 } from '@material-ui/core';
+
+import { Redirect } from 'react-router-dom';
+
 const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
@@ -57,11 +60,27 @@ export default function JobsForm(props) {
 
     const [formObject, setFormObject] = useState({})
 
-    const handleFormData = async (event) => {
-        event.preventDefault();
-        setFormObject(props.dataRequest(event.target))
+    const handleInputChange = (event) => {
+        const { name, value} = event.target;
+        setFormObject({...formObject, [name]: value})
     }
 
+    function handleSubmit(event) {
+        event.preventDefault()
+        props.APIFunction({
+            name: formObject.name,
+            quote: formObject.quote,
+            start: formObject.start,
+            end: formObject.end,
+            contact: formObject.contact,
+            phone: formObject.phone,
+            email: formObject.email,
+            work: formObject.work,
+            notes: formObject.notes
+        })
+            .then(alert("Job created successfully!"))
+            .then(<Redirect to="/admin"></Redirect>)
+    }
 
     return (
         <div className={classes.root}>
@@ -72,11 +91,19 @@ export default function JobsForm(props) {
                 </FormControl> */}
                 <FormControl>
                     <InputLabel htmlFor="jobName">Job Name</InputLabel>
-                    <OutlinedInput id="jobName" name="job_name" className={classes.input} variant="outlined" label="Job Name" />
+                    <OutlinedInput 
+                        id="jobName"
+                        name="name"
+                        onChange={handleInputChange}
+                        className={classes.input}
+                        variant="outlined"
+                        label="Job Name" />
                 </FormControl>
                 <FormControl>
                     <TextField variant="outlined"
-                        id="quote"
+                        id="quoteDate"
+                        name="quote"
+                        onChange={handleInputChange}
                         label="Quote Date"
                         type="date"
                         defaultValue={new Date}
@@ -89,7 +116,9 @@ export default function JobsForm(props) {
                 <div className={classes.break} />
                 <FormControl>
                     <TextField variant="outlined"
-                        id="start"
+                        id="startDate"
+                        name="start"
+                        onChange={handleInputChange}
                         label="Job Start"
                         type="datetime-local"
                         defaultValue={new Date}
@@ -101,7 +130,9 @@ export default function JobsForm(props) {
                 </FormControl>
                 <FormControl>
                     <TextField variant="outlined"
-                        id="end"
+                        id="endDate"
+                        name="end"
+                        onChange={handleInputChange}
                         label="Job End"
                         type="datetime-local"
                         defaultValue={new Date}
@@ -115,22 +146,42 @@ export default function JobsForm(props) {
                 <div className={classes.break} />
                 <Typography variant="body1">Contact Information:</Typography>
                 <FormControl>
-                    <InputLabel htmlFor="contact">Point of Contact</InputLabel>
-                    <OutlinedInput id="contact" name="point_of_contact" className={classes.input} variant="outlined" label="Point of Contact" />
+                    <InputLabel htmlFor="contactName">Point of Contact</InputLabel>
+                    <OutlinedInput
+                        id="contactName" 
+                        name="contact" 
+                        onChange={handleInputChange}
+                        className={classes.input} 
+                        variant="outlined" 
+                        label="Point of Contact" />
                 </FormControl>
                 <FormControl>
-                    <InputLabel htmlFor="phone">Contact Phone</InputLabel>
-                    <OutlinedInput id="phone" name="contact_phone" className={classes.input} variant="outlined" label="Contact Phone" />
+                    <InputLabel htmlFor="contactPhone">Contact Phone</InputLabel>
+                    <OutlinedInput 
+                        id="contactPhone"
+                        name="phone"
+                        onChange={handleInputChange}
+                        className={classes.input}
+                        variant="outlined" 
+                        label="Contact Phone" />
                 </FormControl>
                 <FormControl>
-                    <InputLabel htmlFor="email">Contact Email</InputLabel>
-                    <OutlinedInput id="email" name="contact_email" className={classes.input} variant="outlined" label="Contact Email" />
+                    <InputLabel htmlFor="contactEmail">Contact Email</InputLabel>
+                    <OutlinedInput 
+                        id="contactEmail" 
+                        name="email" 
+                        onChange={handleInputChange}
+                        className={classes.input}
+                        variant="outlined"
+                        label="Contact Email" />
                 </FormControl>
                 <div className={classes.break} />
                 <Typography variant="body1">Scope of Work:</Typography>
                 <FormControl>
                     <TextField
-                        id="work"
+                        id="workDescription"
+                        name="work"
+                        onChange={handleInputChange}
                         className={classes.TextField}
                         multiline
                         rows={4}
@@ -143,6 +194,8 @@ export default function JobsForm(props) {
                 <FormControl>
                     <TextField
                         id="notes"
+                        name="notes"
+                        onChange={handleInputChange}
                         className={classes.TextField}
                         multiline
                         rows={4}
@@ -150,88 +203,10 @@ export default function JobsForm(props) {
                         variant="outlined"
                     />
                 </FormControl>
-                <Button className={classes.button} variant="contained" color="primary" onClick={handleFormData}>
+                <Button className={classes.button} variant="contained" color="primary" onClick={handleSubmit}>
                     Submit
                 </Button>
             </form>
         </div>
     );
 }
-
-
-// export default function JobsForm() {
-//     const classes = useStyles();
-//     return (
-//         <div className={classes.root}>
-//             <form name="job-details">
-//                 {/* <FormControl variant="outlined">
-//                     <InputLabel htmlFor="jobnumber">Job Number</InputLabel>
-//                     <OutlinedInput id="jobnumber" name="job_number" className={classes.input} variant="outlined" label="Job Number" />
-//                 </FormControl> */}
-//                 <FormControl variant="outlined">
-
-//                     {/* <InputLabel htmlFor="quoteDate">Quote Date</InputLabel>
-//                         <OutlinedInput id="quoteDate" name="quote_date" className={classes.input} variant="outlined" label="Quote Date" /> */}
-//                     <TextField
-//                         id="date"
-//                         label="Quote Date"
-//                         type="date"
-//                         defaultValue={new Date}
-//                         className={classes.textField}
-//                         InputLabelProps={{
-//                             shrink: true,
-//                         }}
-//                     />
-//                 </FormControl>
-//                 <TextField
-//                     id="datetime-local"
-//                     label="Appointment Date"
-//                     type="datetime-local"
-//                     defaultValue={new Date}
-//                     className={classes.textField}
-//                     InputLabelProps={{
-//                         shrink: true,
-//                     }}
-//                 />
-//                 <div className={classes.break} />
-//                 <Typography variant="body1">Contact Information:</Typography>
-//                 <FormControl variant="outlined">
-//                     <InputLabel htmlFor="contact">Point of Contact</InputLabel>
-//                     <OutlinedInput id="contact" name="point_of_contact" className={classes.input} variant="outlined" label="Point of Contact" />
-//                 </FormControl>
-//                 <FormControl variant="outlined">
-//                     <InputLabel htmlFor="phone">Contact Phone</InputLabel>
-//                     <OutlinedInput id="phone" name="contact_phone" className={classes.input} variant="outlined" label="Contact Phone" />
-//                 </FormControl>
-//                 <FormControl variant="outlined">
-//                     <InputLabel htmlFor="email">Contact Email</InputLabel>
-//                     <OutlinedInput id="email" name="contact_email" className={classes.input} variant="outlined" label="Contact Email" />
-//                 </FormControl>
-//                 <div className={classes.break} />
-//                 <Typography variant="body1">Scope of Work:</Typography>
-//                 <FormControl variant="outlined">
-//                     <TextField
-//                         id="work"
-//                         className={classes.TextField}
-//                         multiline
-//                         rows={4}
-//                         placeholder="Describe Approved Work"
-//                         variant="outlined"
-//                     />
-//                 </FormControl>
-//                 <div className={classes.break} />
-//                 <Typography variant="body1">Notes:</Typography>
-//                 <FormControl variant="outlined">
-//                     <TextField
-//                         id="notes"
-//                         className={classes.TextField}
-//                         multiline
-//                         rows={4}
-//                         placeholder="Directions, special considerations, etc."
-//                         variant="outlined"
-//                     />
-//                 </FormControl>
-//             </form>
-//         </div>
-//     );
-// }
--- client/src/pages/CreateJob.js
+++ client/src/pages/CreateJob.js
@@ -1,18 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {
     CssBaseline,
     Container,
     Paper,
     Typography,
-    // Button
+    Button
 } from '@material-ui/core';
 
 import MenuToolbar from '../components/MenuToolbar';
 import Copyright from '../components/Copyright';
 import JobsForm from '../components/JobsForm';
-
-import { Redirect } from 'react-router-dom';
 
 import API from '../utils/API';
 
@@ -35,10 +33,10 @@ const useStyles = makeStyles((theme) => ({
     container: {
         margin: theme.spacing(2)
     },
-    // button: {
-    //     margin: theme.spacing(2),
-    //     width: '7rem'
-    // },
+    button: {
+        margin: theme.spacing(2),
+        width: '7rem'
+    },
     paper: {
         padding: theme.spacing(2),
         display: 'flex',
@@ -50,39 +48,6 @@ const useStyles = makeStyles((theme) => ({
 export default function CreateJob() {
     const classes = useStyles()
 
-    const [formObject, setFormObject] = useState({})
-
-    //create a new job using form data, send data to back end
-    function handleSubmit(event) {
-        event.preventDefault()
-        API.createJob({
-            quote: formObject.quote,
-            start: formObject.start,
-            end: formObject.end,
-            contact: formObject.contact,
-            phone: formObject.phone,
-            email: formObject.email,
-            work: formObject.work,
-            notes: formObject.notes
-        })
-            .then(alert("Job created successfully!"))
-            .then(<Redirect to="/admin"></Redirect>)
-    }
-
-    //access formData from child component(JobsForm)
-    const getFormData = (formData) => {
-        setFormObject({...formObject,
-            quote: formData.quote,
-            start: formData.start,
-            end: formData.end,
-            contact: formData.contact,
-            phone: formData.phone,
-            email: formData.email,
-            work: formData.work,
-            notes: formData.notes
-        })
-    }
-
     return (
         <div className={classes.root}>
             <CssBaseline />
@@ -92,10 +57,7 @@ export default function CreateJob() {
                 <Container maxWidth="lg" className={classes.container}>
                     <Paper className={classes.paper}>
                         <Typography variant='h5'>Create New Job</Typography>
-                        <JobsForm dataRequest = {getFormData}/>
-                        {/* <Button className={classes.button} variant="contained" color="primary" onClick={handleSubmit}>
-                            Submit
-                        </Button> */}
+                        <JobsForm APIFunction={API.createJob} />
                     </Paper>
                     <Copyright />
                 </Container>
