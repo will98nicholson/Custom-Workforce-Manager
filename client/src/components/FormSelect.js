8962f6d400fd7a06786cff9e369b2ac343b9ba2a
Corrinne W <corrinneworden@gmail.com>
2021-07-01 20:44:08 -0400

    done debugging front end changes. Dependency added (react-text-mask). Please npm i

---
+++ client/src/components/FormSelect.js
@@ -0,0 +1,57 @@
+import React, { useEffect, MenuProps } from 'react';
+import { 
+    Select,
+    Checkbox,
+    Input,
+    MenuItem,
+    ListItemText
+} from '@material-ui/core';
+
+import API from '../utils/API'
+
+//TODO: API.getAll for services
+//function to push services from db to an array
+//map array to selector
+
+const services = [];
+
+function getServices() {
+    API.getServices()
+        .then((res) => services.map(res._id))
+        .catch((err) => console.log(err))
+}
+
+export default function FormSelect() {
+    const [serviceName, setServiceName] = React.useState([]);
+
+    useEffect(() => {
+        // on load call API for service list
+        getServices()
+    });
+
+    const handleChange = (event) => {
+        setServiceName(event.target.value);
+    };
+
+    return (
+        <div>
+            <Select
+                labelId="select-services"
+                id="demo-mutiple-checkbox"
+                multiple
+                value={serviceName}
+                onChange={handleChange}
+                input={<Input />}
+                renderValue={(selected) => selected.join(', ')}
+                MenuProps={MenuProps}
+            >
+                {services.map((name) => (
+                    <MenuItem key={name} value={name}>
+                        <Checkbox checked={serviceName.indexOf(name) > -1} />
+                        <ListItemText primary={name} />
+                    </MenuItem>
+                ))}
+            </Select>
+        </div>
+    );
+}
---
+++ client/src/components/TextMaskCustom.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import MaskedInput from 'react-text-mask';
+
+export default function TextMaskCustom(props) {
+  const { inputRef, ...other } = props;
+
+  return (
+    <MaskedInput
+      {...other}
+      ref={(ref) => {
+        inputRef(ref ? ref.inputElement : null);
+      }}
+      mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
+      placeholderChar={'\u2000'}
+      showMask
+    />
+  );
+}
