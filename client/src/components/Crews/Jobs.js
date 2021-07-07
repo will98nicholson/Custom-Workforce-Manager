11473bfa1f9977a0114eea1b5fceec102763e9c1
mkotte <74936870+mkotte@users.noreply.github.com>
2021-07-06 20:24:03 -0400

    working crews page

--- client/src/components/Crews/Jobs.js
+++ client/src/components/Crews/Jobs.js
@@ -4,12 +4,17 @@ function Jobs({job}) {
 
     return(
         <>
-            <div className={"job"} id={job._id}>
-                <h2>{job.client.name}</h2>
-                <h3>{job.client.type}</h3>
-                <h4>{job.client.location.city}</h4>
-                <h4>{job.description}</h4>
-                {/* {jobs} */}
+            <div className="job" id={job._id}>
+                <div className="job-header">
+                    <h2 className="job-client-name">{job.client.name}</h2>
+                    <h3 className="job-client-type">{job.client.type}</h3>
+                </div>
+                <div className="location-wrapper">
+                    <h4 className="client-location">{job.client.location.city}</h4>
+                </div>
+                <div className="description-wrapper">
+                    <h4>{job.description}</h4>
+                </div>
             </div>
         </>
     )
--- client/src/components/Crews/List.js
+++ client/src/components/Crews/List.js
@@ -1,17 +1,56 @@
-import { keys } from "@material-ui/core/styles/createBreakpoints";
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
+import Sortable from 'sortablejs';
 import API from "../../utils/API";
 import Jobs from "./Jobs";
 
+
 function List({ crew }) {
-    // if data is returning all jobs
-    // then we need to filter them by crewAssignedToo
-    // and give an option for "unnassigned"
-    // then we need to map the filtered jobs list through the Jobs component
     // TODO :while sorting by position, adding the conditional of comparing lastUpdatedTime
     // TODO :in case jobs have the same position after updating
 
     const [jobs, setJobs] = useState([])
+
+    const listEl = useRef(null)
+    if (listEl.current !== null){
+        const sortable = new Sortable(listEl.current, {
+            group: 'shared',
+            animation: 150,
+            onEnd: function (/**Event*/evt) {
+                var itemEl = evt.item;  // dragged HTMLElement
+                // evt.to;    // target list
+                // evt.from;  // previous list
+                // evt.oldIndex;  // element's old index within old parent
+                // evt.newIndex;  // element's new index within new parent
+                // evt.oldDraggableIndex; // element's old index within old parent, only counting draggable elements
+                // evt.newDraggableIndex; // element's new index within new parent, only counting draggable elements
+                // evt.clone // the clone element
+                
+                console.log(itemEl)
+                // const cardId = evt.to.id
+
+                // const handlePositionSave = () => {
+                //     const newPosition = {
+                //         id: itemEl.id,
+                //         card_id:cardId.substring(3),
+                //     };
+                //     console.log(newPosition)
+                //     savePosition(newPosition)
+                // };
+
+                // const savePosition = (el) => {
+                //     fetch(`/api/tasks/${itemEl.id}`, {
+                //         method: 'PUT',
+                //         headers: {
+                //         'Content-Type': 'application/json',
+                //         },
+                //         body: JSON.stringify(el),
+                //     })
+                // }
+
+                // handlePositionSave();
+            }
+        })
+    }
 
     useEffect(() => {
         loadJobs()
@@ -26,17 +65,18 @@ function List({ crew }) {
   
     return(
         <>
-            <div className={"list"}>
+            <div className="list">
                 <h2>{crew}</h2>
+                <div ref={listEl} calsName="job-list" id={crew + "-jobs"}>
                     {jobs
                     .filter( dataObjs => dataObjs.crewAssignedToo === crew)
                     .sort((a, b) => (a.dailyPosition > b.dailyPosition) ? 1 : -1)
-                    .map( (job, key) => (
-                        <Jobs job={job} id={key}/>
+                    .map( (job) => (
+                        <Jobs job={job} key={job._id}/>
                     ))}
+                </div>    
             </div>
         </>
-
     )
 };
 
